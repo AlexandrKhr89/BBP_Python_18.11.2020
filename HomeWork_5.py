cfba081c36eb05851e03bf6540a8ad9cf3b018c1
Alexanderkhr89 <alexanderkhr89@gmail.com>
2021-01-10 15:14:49 +0200

    HomeWork_5_test.py 3 and 5 task done and send

--- HomeWork_5.py
+++ HomeWork_5.py
@@ -32,33 +32,89 @@ else:
 # Создать список my_result в который вначале поместить
 # элементы на четных местах из my_list_1, а потом все элементы на нечетных местах из my_list_2.
 #
-my_list_1 = (0,1,2,3,4,5)
-my_list_2 = (0,10,20,30,40,50)
+print("\nTask 3a")
 
-my_result = ()
-my_result.apend(my_list_1(::2))
-print(my_result)
-my_result.apend(my_list_2[1::2])
-print(my_result)
+my_list_1 = [0, 1, 2, 3, 4, 5]
+my_list_2 = [0, 15, 20, 35, 40, 55]
+print("my_list_1 = ", my_list_1)
+print("my_list_2 = ", my_list_2)
+# print("Числа из my_list_1 на четных местах : ", my_list_1[::2])
+# print("Числа из my_list_2 на НЕчетных местах : ", my_list_2[1::2])
+my_result = []
 
+for value in my_list_1[::2]:
+    my_result.append(value)
 
+for value in my_list_2[1::2]:
+    my_result.append(value)
 
-
+print("my_result =", my_result)
 
 # 3b. Даны списки my_list_1 и my_list_2. Создать список my_result в который
 # вначале поместить четные элементы (ИМЕННО ЭЛЕМЕНТЫ) из my_list_1 и потом нечетные элементы из my_list_2.
 # my_list_1 = [1,2,3,4,5], my_list_2 = [10, 15, 20, 25] -> my_result [2, 4, 15, 25]
 #
+print("\nTask 3b")
+
+my_list_1 = [6, 7, 8, 9, 10, 11]
+my_list_2 = [10, 11, 12, 13, 14, 15]
+print("my_list_1 = ", my_list_1)
+print("my_list_2 = ", my_list_2)
+
+my_result = []
+
+for value in my_list_1[::]:
+    if value % 2 == 0:
+        my_result.append(value)
+
+for value in my_list_2[::]:
+    if not value % 2 == 0:
+        my_result.append(value)
+
+print("my_result =", my_result)
+
 # 4. Дан список my_list. СОЗДАТЬ НОВЫЙ список new_list у которого первый элемент из my_list
 # стоит на последнем месте. Если my_list [1,2,3,4], то new_list [2,3,4,1]
 #
+print("\nTask 4")
+my_list = [1, 2, 3, 4]
+print("my_list =", my_list)
+new_list = []
+
+for value in my_list[1::1]:
+    new_list.append(value)
+new_list.append(my_list[0])
+print("new_list =", new_list)
+
 # 5.Дан список my_list. В ЭТОМ списке первый элемент переставить на последнее место.
 # [1,2,3,4] -> [2,3,4,1]. Пересоздавать список нельзя! (используйте метод pop)
 #
+print("\nTask 5")
+my_list = [1, 2, 3, 4]
+print("my_list =", my_list)
+my_list.append(my_list.pop(0))
+print("my_list =", my_list)
+
 # 6. Дана строка в которой есть числа (разделяются пробелами).
 # Например "43 больше чем 34 но меньше чем 56". Найти сумму ВСЕХ ЧИСЕЛ (А НЕ ЦИФР) в этой строке.
 # Для данного примера ответ - 133.
+
+print("\nTask 6")
+my_str = "43 больше чем 34 но меньше чем 56"
+
+# for index in my_str:
+    # print(index)
+    # if index.isdigit():
+    #     my_number = index
+    #     print(my_number)
 #
+my_list_from_str = list(my_str)
+print(my_list_from_str)
+
+for index in my_list_from_str:
+    print(index)
+
+
 # 7. Дана строка my_str. Разделите эту строку на пары из двух символов и поместите эти пары в список.
 # Если строка содержит нечетное количество символов, пропущенный второй символ последней пары должен
 # быть заменен подчеркиванием ('_'). Примеры: 'abcd' -> ['ab', 'cd'], 'abcde' -> ['ab', 'cd', e_']
